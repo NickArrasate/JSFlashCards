4bf19140454479c30fc6473af52eb4a8692f99d9
Miles Hewitson and Nick Arrasate <miles@>
2016-03-22 16:15:41 -0700

    add JS and definitions

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+
+
+
+
+  // $(".Functions").click(function(){
+  //   // $(".placeholder").toggle();
+  //   $(".dFunctions").toggle();
+  // });
+  // $(".Functions").click(function(){
+  //   $(".placeholder").toggle();
+  //   $(".dFunctions").toggle();
+  // });
+  // $(".Functions").click(function(){
+  //   $(".placeholder").toggle();
+  //   $(".dFunctions").toggle();
+  // });
+  // $(".Functions").click(function(){
+  //   $(".placeholder").toggle();
+  //   $(".dFunctions").toggle();
+  // });
+  // $(".Functions").click(function(){
+  //   $(".placeholder").toggle();
+  //   $(".dFunctions").toggle();
+  // });
+
+
+
+
+
+
+  //   $(".Javascript").click(function showonlyone(thechosenone) {
+  //      $('.definition').each(function(index) {
+  //           if ($(this).attr("id") == thechosenone) {
+  //                $("#djavascript").show(200);
+  //           }
+  //           else {
+  //                $("#djavascript").hide(600);
+  //           }
+  //      });
+  // };
+
+
+
+
+
+});
